c3ef97e81aa89367cf3491d7486260f4f8f7ab5d
Alexandros Katsaprakakis <a.katsaprakakis@gmail.com>
2017-05-28 10:57:49 +0200

    Update CoreData model to remove unneeded inverse relationships

--- Marvellous/Hero+CoreDataProperties.swift
+++ Marvellous/Hero+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Hero+CoreDataProperties.swift
 //  Marvellous
 //
-//  Created by Alexandros Katsaprakakis on 27/05/2017.
+//  Created by Alexandros Katsaprakakis on 28/05/2017.
 //  Copyright © 2017 Alexandros Katsaprakakis. All rights reserved.
 //
 
@@ -16,46 +16,12 @@ extension Hero {
         return NSFetchRequest<Hero>(entityName: "Hero")
     }
 
-    @NSManaged public var name: String?
-    @NSManaged public var thumbnailUrl: String?
     @NSManaged public var desc: String?
     @NSManaged public var identifier: Int64
     @NSManaged public var modified: NSDate?
-    @NSManaged public var appearsInComics: NSSet?
-    @NSManaged public var appearsInSeries: NSSet?
-
-}
-
-// MARK: Generated accessors for appearsInComics
-extension Hero {
-
-    @objc(addAppearsInComicsObject:)
-    @NSManaged public func addToAppearsInComics(_ value: Comic)
-
-    @objc(removeAppearsInComicsObject:)
-    @NSManaged public func removeFromAppearsInComics(_ value: Comic)
-
-    @objc(addAppearsInComics:)
-    @NSManaged public func addToAppearsInComics(_ values: NSSet)
-
-    @objc(removeAppearsInComics:)
-    @NSManaged public func removeFromAppearsInComics(_ values: NSSet)
-
-}
-
-// MARK: Generated accessors for appearsInSeries
-extension Hero {
-
-    @objc(addAppearsInSeriesObject:)
-    @NSManaged public func addToAppearsInSeries(_ value: Series)
-
-    @objc(removeAppearsInSeriesObject:)
-    @NSManaged public func removeFromAppearsInSeries(_ value: Series)
-
-    @objc(addAppearsInSeries:)
-    @NSManaged public func addToAppearsInSeries(_ values: NSSet)
-
-    @objc(removeAppearsInSeries:)
-    @NSManaged public func removeFromAppearsInSeries(_ values: NSSet)
+    @NSManaged public var name: String?
+    @NSManaged public var thumbnailUrl: String?
+    @NSManaged public var appearsInComics: Comic?
+    @NSManaged public var appearsInSeries: Series?
 
 }
