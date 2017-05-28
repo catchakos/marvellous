f160960bcafde7de2d4d7262d8435be9b024d3e9
Alexandros Katsaprakakis <a.katsaprakakis@gmail.com>
2017-05-28 20:04:41 +0200

    create Navigator class handling UISplitVC

--- Marvellous/Navigator.swift
+++ Marvellous/Navigator.swift
@@ -8,4 +8,26 @@
 
 import UIKit
 
+class Navigator: UISplitViewControllerDelegate {
+    weak var window: UIWindow?
+    
+    func initialSetup() {
+        // Override point for customization after application launch.
+        let splitViewController = window!.rootViewController as! UISplitViewController
+        let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
+        navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem
+        splitViewController.delegate = self
+    }
+    
+    // MARK: - Split view
+    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool {
+        guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
+        guard let topAsDetailController = secondaryAsNavController.topViewController as? HeroDetailViewController else { return false }
+        if topAsDetailController.heroID == nil {
+            // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
+            return true
+        }
+        return false
+    }
+}
 
